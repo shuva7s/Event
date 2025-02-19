7de089116cb5f4195f0a52cef380cc077610f52c
Shuvadeep Mandal <shuvadeepmandal5@gmail.com>
2025-02-19 20:04:15 +0530

    Profile image in Navbar

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,11 +1,28 @@
+import { headers } from "next/headers";
 import { ModeToggle } from "../theme/ModeToggle";
+import { auth } from "@/lib/auth";
+import Dp from "./cards/Dp";
+import Link from "next/link";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth.api.getSession({
+    headers: headers(),
+  });
   return (
     <header className="wrapper">
       <nav className="w-full py-5 flex justify-between items-center">
         <p className="h_md">Evently</p>
-        <ModeToggle />
+        <div className="flex flex-row flex-wrap gap-3 items-center">
+          <ModeToggle />
+          {session && (
+            <>
+              <div className="h-4 w-0.5 rounded-full bg-accent" />
+              <Link href="/profile">
+                <Dp src={session.user.image} name={session.user.name} />
+              </Link>
+            </>
+          )}
+        </div>
       </nav>
     </header>
   );
